1d7491ca85594ae5dd4f3ce986748d245e7931c5
Jordan <jo.dev.wang@gmail.com>
2020-05-30 17:22:41 -0400

    formatting

--- timelec/models.py
+++ timelec/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 class Video(models.Model):
-    name=models.CharField(max_length=500)
-    videofile=models.FileField(upload_to='videos/', null=True)
+    name = models.CharField(max_length=500)
+    videofile = models.FileField(upload_to='videos/', null=True)
 
     def __str__(self):
         return self.name + ": " + str(self.videofile)
-
--- timelec/views.py
+++ timelec/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from .models import Video
 from .forms import VideoForm
 
-def index(request):
 
+def index(request):
     if request.method == 'POST':
         form = VideoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             videofile = Video.objects.filter().order_by('-id')[0]
-            return render(request,'./studentvt/index.html',{
-                        'form': VideoForm(),
-                        'video':videofile
-                        
+            return render(request, './studentvt/index.html', {
+                'form': VideoForm(),
+                'video': videofile
+
             })
     else:
         form = VideoForm()
-        return render(request,'./studentvt/index.html',{
-            'form':form
+        return render(request, './studentvt/index.html', {
+            'form': form
         })
