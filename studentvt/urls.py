bba5f281a418d7d5f438a91f98a897535e791205
Jordan <jo.dev.wang@gmail.com>
2020-05-30 18:34:44 -0400

    BIG MOVES

--- studentvt/urls.py
+++ studentvt/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('convert/', views.transcribe_file_with_word_time_offsets, name="convert")
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- timelec/admin.py
+++ timelec/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Video
- 
+
+
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ['name','videofile']
+    list_display = ['name', 'videofile']
