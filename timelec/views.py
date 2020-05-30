e8ed9655325e1b9af84d6715d811f06fde47d7d1
Xiang Weng <xiang.weng3@gmail.com>
2020-05-30 15:34:22 -0400

    add video function

--- timelec/views.py
+++ timelec/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from .models import Video
+from .forms import VideoForm
 
 def index(request):
-    return render(request,'./studentvt/index.html')
+
+    if request.method == 'POST':
+        form = VideoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            videofile = Video.objects.filter().order_by('-id')[0]
+            return render(request,'./studentvt/index.html',{
+                        'form': VideoForm(),
+                        'video':videofile
+                        
+            })
+    else:
+        form = VideoForm()
+        return render(request,'./studentvt/index.html',{
+            'form':form
+        })
