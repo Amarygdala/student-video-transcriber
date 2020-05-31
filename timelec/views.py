afdcf610190975e1a4ccf208a66ae0db992de187
Jordan <43543240+HotGreenPeas@users.noreply.github.com>
2020-05-31 00:41:15 -0400

    + text

--- timelec/views.py
+++ timelec/views.py
@@ -10,9 +10,7 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file('api-key.json')
 
 
-def transcribe_file_with_word_time_offsets(request):
-    """Transcribe the given audio file synchronously and output the word time
-    offsets."""
+def index(request):
     print("Start")
 
     from google.cloud import speech
@@ -24,57 +22,53 @@ def transcribe_file_with_word_time_offsets(request):
     client = speech.SpeechClient(credentials=credentials)
 
     print("Checked")
-    with io.open('media/videos/mono.flac', 'rb') as audio_file:
-        content = audio_file.read()
 
-    print("audio file read")
-
-    audio = types.RecognitionAudio(content=content)
-
-    print("config start")
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-        language_code='en-US',
-        enable_word_time_offsets=True)
-
-    print("Recognizing:")
-    response = client.recognize(config, audio)
-    print("Recognized")
-
-    for result in response.results:
-        alternative = result.alternatives[0]
-        print('Transcript: {}'.format(alternative.transcript))
-
-        for word_info in alternative.words:
-            word = word_info.word
-            start_time = word_info.start_time
-            end_time = word_info.end_time
-            print('Word: {}, start_time: {}, end_time: {}'.format(
-                word,
-                start_time.seconds + start_time.nanos * 1e-9,
-                end_time.seconds + end_time.nanos * 1e-9))
-
-    return render(request, './studentvt/index.html', {
-            'form': VideoForm(),
-            'video': Video.objects.filter().order_by('-id')[0]
-
-        })
-
-
-def index(request):
     if request.method == 'POST':
         form = VideoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             videofile = Video.objects.filter().order_by('-id')[0]
             videopath = str(videofile.videofile)
-            audio = AudioSegment.from_file("./media/"+videopath,"mp4")
-            audio.export("./media/videos/"+str(videofile.name)+".wav", format="wav")
-            #convert audio to text here
+            audio = AudioSegment.from_file("./media/" + videopath, "mp4")
+            audio.export("./media/videos/" + str(videofile.name) + ".wav", format="wav")
+            # convert audio to text here
+
+            # convert audio to text here
+            with io.open('media/videos/mono.flac', 'rb') as audio_file:
+                content = audio_file.read()
+
+            print("audio file read")
+
+            audio = types.RecognitionAudio(content=content)
+
+            print("config start")
+            config = types.RecognitionConfig(
+                encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+                language_code='en-US',
+                enable_word_time_offsets=True)
+
+            print("Recognizing:")
+            response = client.recognize(config, audio)
+            print("Recognized")
+
+            text = ""
+            for result in response.results:
+                alternative = result.alternatives[0]
+                text += ('Transcript: {}'.format(alternative.transcript))
+
+                for word_info in alternative.words:
+                    word = word_info.word
+                    start_time = word_info.start_time
+                    end_time = word_info.end_time
+                    text += ('Word: {}, start_time: {}, end_time: {}'.format(
+                        word,
+                        start_time.seconds + start_time.nanos * 1e-9,
+                        end_time.seconds + end_time.nanos * 1e-9))
 
             return render(request, './studentvt/index.html', {
                 'form': VideoForm(),
-                'video': videofile
+                'video': videofile,
+                'text': text,
 
             })
     else:
